3918baaec377d59c1f55ad0cc4fd3fe6dcf1c773
Elena Kim <elena.kim3011@gmail.com>
2023-03-23 16:54:31 +0100

    feat: add basic authorization for 'import' call

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
 import { ApiService } from '../../core/api.service';
 import { switchMap } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
@@ -32,11 +33,28 @@ export class ManageProductsService extends ApiService {
   private getPreSignedUrl(fileName: string): Observable<string> {
     const url = this.getUrl('import', 'import');
 
+    const basicAuth = this.getBasicAuth();
+
     return this.http.get(url, {
       params: {
         name: fileName,
       },
       responseType: 'text',
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      headers: new HttpHeaders(basicAuth ? { Authorization: basicAuth } : {}),
     });
+  }
+
+  private getBasicAuth(): string | null {
+    const authorizationToken =
+      localStorage.getItem('authorization_token') || '';
+
+    if (!authorizationToken) {
+      return null;
+    }
+
+    const C_USERNAME = 'relnofollow';
+    const base64Credentials = btoa(`${C_USERNAME}:${authorizationToken}`);
+    return `Basic ${base64Credentials}`;
   }
 }
